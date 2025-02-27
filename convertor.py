ab543c56fcc64dc696fc5dedfa59fe93e4c2406d
sana <161457792+sanajameel1@users.noreply.github.com>
2025-02-27 19:49:42 +0500

    Add files via upload

---
+++ convertor.py
@@ -0,0 +1,116 @@
+import streamlit as st
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #1e1e2f;
+        color: white;
+    }
+    .seApp {
+        background: linear-gradient(135deg,rgb(79, 59, 59),rgb(87, 126, 160));
+         padding: 30px;
+        border-radius: 15px;
+        box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.3)
+    }
+    h1 {
+        text-align: center;
+        font-size: 36px;
+        color: white;
+    }
+    .stButton>button{
+        background: linear-gradient(135deg,rgba(4, 77, 92, 0.48), #0088cc);
+        color: white;
+        font-size: 18px;
+        padding: 10px 20px;
+        border-radius: 10px;
+        transition: 0.3s;
+        box-shadow: 0px 5px 15px rgba(0, 201, 255, 0.4);
+    }
+    .stButton>button:hover {
+        transform: scale(1.05);
+        background: linear-gradient(45deg,rgb(89, 132, 214),rgb(6, 68, 86));
+        color: black;
+    }
+    .result-box {
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+        background: rgba(255, 255, 255, 0.1);
+        padding: 25px;
+        border-radius: 10px;
+        margin-top: 20px;
+        box-shadow: 0px 5px 15px rgba(0, 201, 255, 0.3);
+    }
+    .footer{
+        text-align: center;
+        margin-top: 50px;
+        color: black;
+        font-size: 14px;
+    }
+    </style>
+     """,
+     unsafe_allow_html=True
+)
+
+#title and Description:
+st.markdown("<h1>🚀 Universal Unit Converter </h1>", unsafe_allow_html=True)
+st.write("Easily convert between different units of lenght, weight, temperature.")
+
+#sidebar menu
+conversion_type = st.sidebar.selectbox("Select Conversion Type", ["Length", "Weight", "Temperature"])
+value =st.number_input("Enter Value", value=0.0, step=0.1)
+col1, col2 = st.columns(2)
+
+if conversion_type == "Length":
+    with col1:
+        from_unit = st.selectbox("From",  ["Meters", "Kilograms", "Centimeters", "Milimeters", "Miless", "Yard", "Inches", "Feed"])
+    with col2:
+        to_unit = st.selectbox("To", ["Meters", "Kilograms", "Centimeters", "Milimeters", "Miless", "Yard", "Inches", "Feed"])
+elif conversion_type == "Weight":
+    with col1:
+        from_unit = st.selectbox("From", ["Kilograms", "Grams", "Pounds", "Ounces"])
+    with col2:
+        to_unit = st.selectbox("To", ["Kilograms", "Grams", "Milligrams" ,"Pounds", "Ounces"])
+elif conversion_type == "Temperature":
+    with col1:
+        from_unit = st.selectbox("From", ["Celsius", "Fahrenheit", "Kelvin"])
+    with col2:
+        to_unit = st.selectbox("To", ["Celsius", "Fahrenheit", "Kelvin"])
+
+#conversion logic
+def length_convertor(value, from_unit, to_unit):
+    length_units = {
+        "Meters": 1,  "Kilograms": 0.001, "Centimeters": 100, "Milimeters": 1000,
+         "Miles": 0.000621371, "Yards": 1.09361, "Inches": 39.37, "Feed": 3.28
+    }
+    return (value / length_units[from_unit]) * length_units[to_unit]
+
+def weight_convertor(value, from_unit, to_unit):
+    weight_units = {
+        "Kilograms": 1, "Grams": 1000, "Milligrams": 1000000, "Pounds": 2.20462, "Ounces": 35.274
+    }
+    return (value / weight_units[from_unit]) * weight_units[to_unit]
+    
+def temperature_convertor(value, from_unit, to_unit):
+    if from_unit == "Celsius":
+        return (value * 9/5 + 32) if to_unit == "Fahrenheit" else (value + 273.15) if to_unit == "Kelvin" else value
+    elif from_unit == "Fahrenheit":
+        return (value - 32) * 5/9 if to_unit == "Celsius" else (value - 32) * 5/9 + 273.15 if to_unit == "Kelvin" else value
+    elif from_unit == "Kelvin":
+        return (value - 273.15) if to_unit == "Celsius" else (value - 273.15) * 9/5 + 32 if to_unit == "Fahrenheit" else value
+    return value
+
+    #conversion button
+if st.button("Convert"):
+    if conversion_type == "Length":
+        result = length_convertor(value, from_unit, to_unit)
+    elif conversion_type == "Weight":
+        result = weight_convertor(value, from_unit, to_unit)
+    elif conversion_type == "Temperature":
+        result = temperature_convertor(value, from_unit, to_unit)
+
+    st.markdown(f"<div class='result-box'>{value} {from_unit} = {result:.4f} {to_unit}</div>", unsafe_allow_html=True)
+
+st.markdown("<div class='footer'>Developed by <a href='https://github.com/sanajameel1'>@sanajameel1</a></div>", unsafe_allow_html=True)
+
+
